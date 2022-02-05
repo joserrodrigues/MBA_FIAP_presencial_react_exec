79ef7063ad22810ce6f5e9b9e60258ff37ff66a9
rubens Rodrigues <rubens@schoolguardian.app>
2022-02-04 22:13:34 -0300

    Add Exec 2 - Aula 4

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import serviceWorker from './serviceworker';
 
 import { BrowserRouter } from "react-router-dom";
 import Router from "./Routes/routes";
@@ -21,3 +22,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.registerServiceWorker();
